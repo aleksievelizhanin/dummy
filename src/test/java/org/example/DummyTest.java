387cb53256a19481be1a0822ce99886ff68f72d3
Aleksei Velizhanin <aleksei@codeborne.com>
2025-06-27 16:26:08 +0300

    myservice -> localhost

--- src/test/java/org/example/DummyTest.java
+++ src/test/java/org/example/DummyTest.java
@@ -16,7 +16,7 @@ public class DummyTest {
 
         String targetUrl = System.getenv().getOrDefault(
                 "TARGET_URL",
-                "http://myservice:8080/action"
+                "http://localhost:8080/action"
         );
 
         open(targetUrl);
