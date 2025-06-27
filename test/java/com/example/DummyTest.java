d72a6fbbec198ecfca3eb5830d7a2511fe659d8c
Aleksei Velizhanin <aleksei@codeborne.com>
2025-06-27 14:32:14 +0300

    Test selenide inside githubactions

---
+++ test/java/com/example/DummyTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class DummyTest {
+
+  @Test
+  void callServiceAction() {
+    // Pick up env vars set in the workflow
+    Configuration.browser  = System.getenv().getOrDefault("SELENIDE_BROWSER", "chrome");
+    Configuration.headless = Boolean.parseBoolean(System.getenv().getOrDefault("SELENIDE_HEADLESS", "true"));
+
+    String targetUrl = System.getenv().getOrDefault(
+        "TARGET_URL",
+        "http://myservice:8080/action"
+    );
+
+    open(targetUrl);
+
+    // Ultra-simple smoke assertion: page has a <body>
+    $("body").shouldBe(Condition.exist);
+  }
+}
+
